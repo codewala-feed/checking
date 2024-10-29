e70ef6e3c54478d51e9fdbd7fb9f32a9da7dbbfc
nikhil <nikhilpokuri143@gmail.com>
2024-10-29 23:07:32 +0530

    database added

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from database import insert_data
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def homepage():
     return {"response":"Hey Coder, You are in homepage :)"}
+
+@app.route("/create", methods=["POST"])
+def create_user():
+    user = request.json
+    return insert_data(user)
---
+++ database.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+host = "ocdb.app"
+port = 5050
+database = "db_42vjh6bg4" # your database
+username = "user_42vjh6bg4" # your username
+password = "p42vjh6bg4" # your password
+
+connection_str = f"mongodb://{username}:{password}@{host}:{port}/{database}"
+
+my_client = MongoClient(connection_str)
+my_db = my_client[database]
+results = my_db["results"]
+
+def insert_data(raw):
+    results.insert_one(raw)
+    return "Data Inserted :)"
